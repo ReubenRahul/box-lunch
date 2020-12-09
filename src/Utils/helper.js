4c9cc50ddf1bd269a9b23cbc9924640fb52cfa5b
Reuben Rahul <reuben.rahul@ithands.biz>
2020-12-09 18:34:59 +0530

    hotfix(add-rotational-test): add rotational test

--- src/Utils/helper.js
+++ src/Utils/helper.js
@@ -1,14 +1,23 @@
-const convertDate = (date) => { 
+const convertDate = (date) => {
     const dd = new Date(date);
-    
-    date = [ 
-        dd.getFullYear(), 
+
+    date = [
+        dd.getFullYear(),
         ('0' + (dd.getMonth() + 1)).slice(-2),
-        ('0' + dd.getDate()).slice(-2) 
+        ('0' + dd.getDate()).slice(-2)
     ].join('-');
     return date;
 }
 
+const getDateTimestamps = (date) => {
+   const dateTime =  new Date( date );
+   return dateTime.getTime();
+}
+
+const getDateFromTimestamps = timeStamps => new Date(parseInt(timeStamps) );
+
 export {
-    convertDate
+    convertDate,
+    getDateTimestamps,
+    getDateFromTimestamps
 }
